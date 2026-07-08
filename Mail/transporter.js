32a74a54ed1b3dc9c366cd35ca7b4b7778c297ce
celebrity4000 <raysonali115@gmail.com>
2026-07-08 16:16:32 +0530

    resend

--- Mail/transporter.js
+++ Mail/transporter.js
@@ -1,16 +1,8 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
 class EmailService {
   constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false, // Use TLS
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+    this.resend = new Resend(process.env.RESEND_API_KEY);
   }
 
   async sendOTP(email, otp) {
@@ -19,21 +11,19 @@ class EmailService {
       return;
     }
 
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "This is from GSB: Your OTP",
-      text: `Your OTP is: ${otp}`,
-      html: `<div style="font-family: Arial, sans-serif; text-align: left;">
-             <h2 style="color:rgb(255, 215, 0);">Your OTP Code</h2>
-             <p style="font-size: 16px;">Your OTP is: <strong>${otp}</strong></p>
-             <p style="font-size: 14px; color: #888;">Please use this code to complete your verification.</p>
-           </div>`,
-    };
-
     try {
-      const info = await this.transporter.sendMail(mailOptions);
-      console.log("Email sent successfully: ", info.response);
+      const info = await this.resend.emails.send({
+        from: process.env.EMAIL_FROM,
+        to: email,
+        subject: "This is from GSB: Your OTP",
+        text: `Your OTP is: ${otp}`,
+        html: `<div style="font-family: Arial, sans-serif; text-align: left;">
+               <h2 style="color:rgb(255, 215, 0);">Your OTP Code</h2>
+               <p style="font-size: 16px;">Your OTP is: <strong>${otp}</strong></p>
+               <p style="font-size: 14px; color: #888;">Please use this code to complete your verification.</p>
+             </div>`,
+      });
+      console.log("Email sent successfully: ", info);
     } catch (error) {
       console.error("Error sending email: ", error);
     }
